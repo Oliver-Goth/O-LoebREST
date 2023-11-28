d631fca01fc93d755e1d1f03b4cc06a5fc84de1c
Oliver-Goth <Olivergoth00@gmail.com>
2023-11-28 10:58:28 +0100

    Created RESTTest

--- O-LoebREST/Class/Model.cs
+++ O-LoebREST/Class/Model.cs
@@ -1,6 +1,15 @@
-﻿namespace O_LoebREST.Class
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace O_LoebREST.Class
 {
     public class Model
     {
+        public string Name { get; set; }
+        public string age { get; set; }
+
+        public void validate()
+        {
+
+        }
     }
 }
