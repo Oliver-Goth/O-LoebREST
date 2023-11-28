f93966e8375f7a8bdcb8448e639f223a8cb9c96b
Oliver-Goth <Olivergoth00@gmail.com>
2023-11-28 11:23:51 +0100

    new resttest

---
+++ O-LoebRESTRESTTests/Models/ModelRESTTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using O_LoebREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O_LoebREST.Class.RESTTests
+{
+    [TestClass()]
+    public class ModelRESTTests
+    {
+        [TestMethod()]
+        public void validateRESTTests()
+        {
+            Assert.Fail();
+        }
+    }
+}
