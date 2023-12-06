cfb270a751e6f39f10bf84f8e5c186dd991c0e4c
nhthomsen <nooblas@hotmail.com>
2023-12-06 11:00:04 +0100

    Added model to answer

---
+++ O-LoebREST/Models/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace O_LoebREST.Models
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string AnswerToQuestion { get; set; }
+        public bool IsCorrectAnswer { get; set; }
+        public int QuestionId { get; set; }
+    }
+}
