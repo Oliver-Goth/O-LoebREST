0a4fd6bd88724631ed4f0d86bd4f86e511a5303f
nhthomsen <nooblas@hotmail.com>
2023-12-07 10:58:05 +0100

    Added repo for GPSLocation

---
+++ O-LoebREST/Repository/GPSLocationRepoDB.cs
@@ -0,0 +1,19 @@
+﻿using O_LoebREST.DBContext;
+using O_LoebREST.Models;
+
+namespace O_LoebREST.Repository
+{
+    public class GPSLocationRepoDB
+    {
+        private readonly DataBaseContext _context;
+        public GPSLocationRepoDB(DataBaseContext context) 
+        {
+            _context = context;
+        }
+        public void AddGPSLocation(GPSLocation gpsLocation) 
+        {
+            _context.GPSLocations.Add(gpsLocation);
+            _context.SaveChanges();
+        }
+    }
+}
